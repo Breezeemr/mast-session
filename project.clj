c10c4b54d8f5c8c51ba57268073cff4a649e6c1e
dan sutton <dsutton@breezeehr.com>
2017-08-22 10:39:07 -0500

    Expose session and is-authorized to web

--- project.clj
+++ project.clj
@@ -7,4 +7,10 @@
                  [buddy/buddy-sign "2.0.0"]
                  [clj-time "0.14.0"]
                  [clj-http "3.7.0"]
-                 [cheshire "5.8.0"]])
+                 [cheshire "5.8.0"]
+
+                 [compojure "1.5.1"]
+                 [ring/ring-defaults "0.2.1"]
+                 [ring/ring-json "0.4.0"]]
+  :plugins [[lein-ring "0.12.0"]]
+  :ring {:handler mast-session.handler/app})
